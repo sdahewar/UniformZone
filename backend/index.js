0c900188fc36e230ae0ea5d0ff74fd1861ce4171
sdahewar <dsk7.work@gmail.com>
2024-08-06 23:05:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ const upload = multer({ storage: storage });
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `https://uniform-zone.vercel.app/images/${req.file.filename}`,
   });
 });
 app.use("/images", express.static("upload/images"));
